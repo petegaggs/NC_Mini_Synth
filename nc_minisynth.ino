eca7dd62a78c57cfcae057b1f0c5b5b1a6de545b
petegaggs <35112776+petegaggs@users.noreply.github.com>
2019-07-28 19:57:36 +0100

    Add files via upload

--- nc_minisynth.ino
+++ nc_minisynth.ino
@@ -39,7 +39,12 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 #include <avr/io.h> 
 #include <avr/interrupt.h>
 #define TIMER_PIN 9 //OC1A output
-#define INTEGRATOR_PWM_PIN 3 //integrator drive by timer2 PWM OC2B, for mk2 h/w
+#define INTEGRATOR_PWM_PIN 3 //integrator drive by timer2 PWM OC2B
+#define INTEGRATOR_PWM OCR2B
+#define LFO_PWM_PIN 11 //LFO by timer2 PWM OC2A
+#define LFO_PWM OCR2A
+#define LFO_FREQ_PIN A1
+#define LFO_WAVE_PIN A0
 #define GATE_PIN 8 //gate control
 #define NOISE_PIN 7 //psuedo random noise output
 #define MIDI_BASE_NOTE 21 //lowest midi note
@@ -55,6 +60,31 @@ uint16_t pwmVal; //value for PWM with 16 bit precision, dither will be used to g
 float bendControlVoltage = 0; // represents midi pitch bend
 uint32_t lfsr = 1; //32 bit LFSR, must be non-zero to start
 
+// table of 256 sine values / one sine period / stored in flash memory
+const char sineTable[] PROGMEM = {
+  127,130,133,136,139,143,146,149,152,155,158,161,164,167,170,173,176,178,181,184,187,190,192,195,198,200,203,205,208,210,212,215,217,219,221,223,225,227,229,231,233,234,236,238,239,240,
+  242,243,244,245,247,248,249,249,250,251,252,252,253,253,253,254,254,254,254,254,254,254,253,253,253,252,252,251,250,249,249,248,247,245,244,243,242,240,239,238,236,234,233,231,229,227,225,223,
+  221,219,217,215,212,210,208,205,203,200,198,195,192,190,187,184,181,178,176,173,170,167,164,161,158,155,152,149,146,143,139,136,133,130,127,124,121,118,115,111,108,105,102,99,96,93,90,87,84,81,78,
+  76,73,70,67,64,62,59,56,54,51,49,46,44,42,39,37,35,33,31,29,27,25,23,21,20,18,16,15,14,12,11,10,9,7,6,5,5,4,3,2,2,1,1,1,0,0,0,0,0,0,0,1,1,1,2,2,3,4,5,5,6,7,9,10,11,12,14,15,16,18,20,21,23,25,27,29,31,
+  33,35,37,39,42,44,46,49,51,54,56,59,62,64,67,70,73,76,78,81,84,87,90,93,96,99,102,105,108,111,115,118,121,124
+};
+
+// LFO stuff
+uint32_t lfoPhaccu;   // phase accumulator
+uint32_t lfoTword_m;  // dds tuning word m
+uint8_t lfoCnt;      // top 8 bits of accum is index into table
+
+float lfoControlVoltage;
+enum lfoWaveTypes {
+  RAMP,
+  SAW,
+  TRI,
+  SINE,
+  SQR
+};
+lfoWaveTypes lfoWaveform;
+bool lfoSyncMode = false;
+bool lfoReset = false;
 void setTimer1(uint16_t val) {
   OCR1AH = val >> 8;
   OCR1AL = val;
@@ -79,8 +109,9 @@ void setup() {
   TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS11);
  
   pinMode(INTEGRATOR_PWM_PIN, OUTPUT); // OC2B output
-  // timer 2 phase accurate PWM, no prescaling, non inverting mode channel B
-  TCCR2A = _BV(COM2B1) | _BV(WGM20);
+  pinMode(LFO_PWM_PIN, OUTPUT); //OC2A output
+  // timer 2 phase accurate PWM, no prescaling, non inverting mode channel A+B
+  TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM20);
   TCCR2B = _BV(CS20);
   // timer 2 interrupts (used for noise)
   TIMSK2 = _BV(TOIE2);
@@ -88,8 +119,28 @@ void setup() {
   setNotePitch(60); //middle C for test
 }
 
+void getLfoParams() {
+  // read ADC to calculate the required DDS tuning word, log scale between 0.01Hz and 10Hz approx
+  float lfoControlVoltage = float(analogRead(LFO_FREQ_PIN)) * float(10)/float(1024); //gives 10 octaves range 0.01Hz to 10Hz
+  lfoTword_m = float(1369) * pow(2.0, lfoControlVoltage); //1369 sets the lowest frequency to 0.01Hz
+  // read ADC to get the LFO wave type
+  int adcVal = analogRead(LFO_WAVE_PIN);
+  if (adcVal < 128) {
+    lfoWaveform = RAMP;
+  } else if (adcVal < 384) {
+    lfoWaveform = SAW;
+  } else if (adcVal < 640) {
+    lfoWaveform = TRI;
+  } else if (adcVal < 896) {
+    lfoWaveform = SINE;
+  } else {
+    lfoWaveform = SQR;
+  }
+}
+
 void loop() {
   MIDI.read();
+  getLfoParams();
 }
 
 SIGNAL(TIMER2_OVF_vect) {
@@ -113,7 +164,43 @@ SIGNAL(TIMER2_OVF_vect) {
   if (pwmFrac > ditherByte) {
     pwmSet += 1; // increase, ensure no overflow
   }
-  OCR2B = pwmSet; // write to pwm register
+  INTEGRATOR_PWM = pwmSet; // write to pwm register
+  // handle LFO DDS
+  if (lfoReset) {
+    lfoPhaccu = 0; // reset the lfo
+    lfoReset = false;
+  } else {
+    lfoPhaccu += lfoTword_m; // increment phase accumulator  
+  }
+  lfoCnt = lfoPhaccu >> 24;  // use upper 8 bits for phase accu as frequency information
+  switch (lfoWaveform) {
+    case RAMP:
+      LFO_PWM = lfoCnt;
+      break;
+    case SAW:
+      LFO_PWM = 255 - lfoCnt;
+      break;
+    case TRI:
+      if (lfoCnt & 0x80) {
+        LFO_PWM = 254 - ((lfoCnt & 0x7F) << 1); //ramp down
+      } else {
+        LFO_PWM = lfoCnt << 1; //ramp up
+      }
+      break;
+    case SINE:
+      // sine wave from table
+      LFO_PWM = pgm_read_byte_near(sineTable + lfoCnt);
+      break;
+    case SQR:
+      if (lfoCnt & 0x80) {
+        LFO_PWM = 255;
+      } else {
+        LFO_PWM = 0;
+      }
+      break;
+    default:
+      break;
+  }
 }
 
 void handleNoteOn(byte channel, byte pitch, byte velocity) { 
